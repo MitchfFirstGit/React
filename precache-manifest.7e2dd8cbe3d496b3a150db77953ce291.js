8e3daeecc3ed6a61b0a8128ea5b8a64ed11817ac
Mitch <artfixmitch@gmail.com>
2019-05-05 23:25:29 +0300

    Updates

---
+++ precache-manifest.7e2dd8cbe3d496b3a150db77953ce291.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "27980199a02d12b927ce",
+    "url": "/React/static/js/main.cd5ae4c7.chunk.js"
+  },
+  {
+    "revision": "a646cf0c0079894fc924",
+    "url": "/React/static/js/2.63aac54b.chunk.js"
+  },
+  {
+    "revision": "27980199a02d12b927ce",
+    "url": "/React/static/css/main.94ded612.chunk.css"
+  },
+  {
+    "revision": "07af631356382c90ddc58a70cd460e27",
+    "url": "/React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React/precache-manifest.de9835a347c03a4747b7f54b83b5178f.js"
+  "/React/precache-manifest.7e2dd8cbe3d496b3a150db77953ce291.js"
 );
 
 workbox.clientsClaim();
